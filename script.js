5091a4e4e1cbca6d8d410970f839fba8cca39fe7
Anas Khafaga <anaskhafaga1810@gmail.com>
2021-11-25 22:53:25 +0200

    components in Action

--- script.js
+++ script.js
@@ -3,39 +3,59 @@ const player1 = 'X'
 const player2 = 'O'
 let winner = 'X';
 
-const Row1 = (
-  <div className="row g-0">
-      <div className="col-4 tl"></div>
-      <div className="col-4 tm"></div>
-      <div className="col-4 tr"></div>
-  </div>
-)
+class Row1 extends React.Component {
+  constructor() {
+    super()
+  }
 
-let Row2;
-if(winner == 'O') {
-  Row2 = (
-    <div className="row g-0">
-        <div className="col-4 ml">{player1}</div>
-        <div className="col-4 mm">{player1}</div>
-        <div className="col-4 mr">{player1}</div>
-    </div>
-  )
-}else {
-  Row2 = (
-    <div className="row g-0">
-        <div className="col-4 ml">{player2}</div>
-        <div className="col-4 mm">{player2}</div>
-        <div className="col-4 mr">{player2}</div>
-    </div>
-  )
+  render() {
+    return (
+      <div className="row g-0">
+          <div className="col-4 tl"></div>
+          <div className="col-4 tm"></div>
+          <div className="col-4 tr"></div>
+      </div>
+    )
+  }
 }
 
-const Row3 = (
-  <div className="row g-0">
-      <div className="col-4 bl"></div>
-      <div className="col-4 bm"></div>
-      <div className="col-4 br"></div>
-  </div>
-)
+function Row2() {
 
-ReactDOM.render([Row1, Row2, Row3], boardRoot)
+  if(winner == 'X'){
+    return (
+      <div className="row g-0">
+          <div className="col-4 ml">{player1}</div>
+          <div className="col-4 mm">{player1}</div>
+          <div className="col-4 mr">{player1}</div>
+      </div>
+    )
+
+  }else {
+    return (
+      <div className="row g-0">
+          <div className="col-4 ml">{player2}</div>
+          <div className="col-4 mm">{player2}</div>
+          <div className="col-4 mr">{player2}</div>
+      </div>
+    )
+  }
+
+};
+
+class Row3 extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="row g-0">
+          <div className="col-4 bl"></div>
+          <div className="col-4 bm"></div>
+          <div className="col-4 br"></div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render([<Row1 />, <Row2 />, <Row3 />], boardRoot)
