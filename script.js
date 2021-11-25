36a284fd69a6d9fea7f0de2bef351b593678d2a6
Anas Khafaga <anaskhafaga1810@gmail.com>
2021-11-26 00:18:43 +0200

    compose and extract in action

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const boardRoot = document.querySelector('#board-root');
+const boardContainerRoot = document.querySelector('#board-container-root');
 const player1 = 'X'
 const player2 = 'O'
 let winner = 'X';
+
+function Block(){
+  return (
+    <div className="col-4 tl"></div>
+  )
+}
 
 class Row1 extends React.Component {
   constructor() {
@@ -11,9 +17,12 @@ class Row1 extends React.Component {
   render() {
     return (
       <div className="row g-0">
-          <div className="col-4 tl"></div>
+          {/*<div className="col-4 tl"></div>
           <div className="col-4 tm"></div>
-          <div className="col-4 tr"></div>
+          <div className="col-4 tr"></div>*/}
+          <Block />
+          <Block />
+          <Block />
       </div>
     )
   }
@@ -24,18 +33,24 @@ function Row2() {
   if(winner == 'X'){
     return (
       <div className="row g-0">
-          <div className="col-4 ml">{player1}</div>
+          {/*<div className="col-4 ml">{player1}</div>
           <div className="col-4 mm">{player1}</div>
-          <div className="col-4 mr">{player1}</div>
+          <div className="col-4 mr">{player1}</div>*/}
+          <Block />
+          <Block />
+          <Block />
       </div>
     )
 
   }else {
     return (
       <div className="row g-0">
-          <div className="col-4 ml">{player2}</div>
+          {/*<div className="col-4 ml">{player2}</div>
           <div className="col-4 mm">{player2}</div>
-          <div className="col-4 mr">{player2}</div>
+          <div className="col-4 mr">{player2}</div>*/}
+          <Block />
+          <Block />
+          <Block />
       </div>
     )
   }
@@ -50,12 +65,31 @@ class Row3 extends React.Component {
   render() {
     return (
       <div className="row g-0">
-          <div className="col-4 bl"></div>
+          {/*<div className="col-4 bl"></div>
           <div className="col-4 bm"></div>
-          <div className="col-4 br"></div>
+          <div className="col-4 br"></div>*/}
+          <Block />
+          <Block />
+          <Block />
       </div>
     )
   }
 }
 
-ReactDOM.render([<Row1 />, <Row2 />, <Row3 />], boardRoot)
+class Board extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <div className="bk-color-2 w-100 h-75 grid board row g-0" id="board-root">
+        <Row1 />
+        <Row2 />
+        <Row3 />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Board />, boardContainerRoot)
