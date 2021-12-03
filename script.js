6c4c60f833d5d26c8feb8288179dc9d5fe902fc2
Anas Khafaga <anaskhafaga1810@gmail.com>
2021-12-04 00:40:31 +0200

    state in Action II

--- script.js
+++ script.js
@@ -2,79 +2,22 @@ const boardContainerRoot = document.querySelector('#board-container-root');
 const resultContainerRoot = document.querySelector('#results')
 const player1 = 'X'
 const player2 = 'O'
-let winner = 'X';
 
-function Block({classProp}){
-
-  function checkBlock(e, player) {
-    e.target.textContent = player
-  }
+function Block({classProp, click}){
 
   return (
-    <div className={classProp} onClick={(e) => checkBlock(e, 'X')}></div>
+    <div className={classProp} onClick={click}></div>
   )
-}
-
-class Row1 extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const {classProp} = this.props
-    return (
-      <div className={classProp}>
-          <Block classProp="col-4 tl" />
-          <Block classProp="col-4 tm" />
-          <Block classProp="col-4 tr" />
-      </div>
-    )
-  }
-}
-
-function Row2({classProp}) {
-
-  if(winner == 'X'){
-    return (
-      <div className={classProp}>
-          <Block classProp="col-4 ml" />
-          <Block classProp="col-4 mm" />
-          <Block classProp="col-4 mr" />
-      </div>
-    )
-
-  }else {
-    return (
-      <div className={classProp}>
-          <Block classProp="col-4 ml" />
-          <Block classProp="col-4 mm" />
-          <Block classProp="col-4 mr" />
-      </div>
-    )
-  }
-
-};
-
-class Row3 extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const {classProp} = this.props
-    return (
-      <div className={classProp}>
-          <Block classProp="col-4 bl" />
-          <Block classProp="col-4 bm" />
-          <Block classProp="col-4 br" />
-      </div>
-    )
-  }
 }
 
 class Board extends React.Component {
   constructor() {
     super()
+
+    // define a state
+    this.state = {
+      currentPlayer: 'X'
+    }
   }
 
   toggleColor(classParam, e) {
@@ -83,12 +26,45 @@ class Board extends React.Component {
     console.log(this);
   }
 
+  checkBlock(e) {
+
+    // update the state
+
+    // check block is checked or not
+    if(e.target.textContent) return;
+
+    // update the block if not
+    e.target.textContent = this.state.currentPlayer
+
+    // update the game state to next player
+    this.setState((state) => {
+      if(state.currentPlayer == 'X'){
+        return {currentPlayer: 'O'}
+      }else{
+        return {currentPlayer: 'X'}
+      }
+    })
+
+  }
+
   render() {
     return(
       <div className="bk-color-2 w-100 h-75 grid board row g-0" id="board-root" onClick={this.toggleColor.bind(this, 'bk-color-3')}>
-        <Row1 classProp="row g-0" />
-        <Row2 classProp="row g-0" />
-        <Row3 classProp="row g-0" />
+        <div className="row g-0">
+            <Block classProp="col-4 tl" click={this.checkBlock.bind(this)} />
+            <Block classProp="col-4 tm" click={this.checkBlock.bind(this)} />
+            <Block classProp="col-4 tr" click={this.checkBlock.bind(this)} />
+        </div>
+        <div className="row g-0">
+            <Block classProp="col-4 ml" click={this.checkBlock.bind(this)} />
+            <Block classProp="col-4 mm" click={this.checkBlock.bind(this)} />
+            <Block classProp="col-4 mr" click={this.checkBlock.bind(this)} />
+        </div>
+        <div className="row g-0">
+            <Block classProp="col-4 bl" click={this.checkBlock.bind(this)} />
+            <Block classProp="col-4 bm" click={this.checkBlock.bind(this)} />
+            <Block classProp="col-4 br" click={this.checkBlock.bind(this)} />
+        </div>
       </div>
     )
   }
@@ -96,10 +72,10 @@ class Board extends React.Component {
 
 function ResultBlock({player, score, click}) {
   return (
-    <div class="row g-0">
-        <div class="col-4">{player}</div>
-        <div class="col-4"><img src="heart.png" alt="" onClick={click} /></div>
-        <div class="col-4">{score}</div>
+    <div className="row g-0">
+        <div className="col-4">{player}</div>
+        <div className="col-4"><img src="heart.png" alt="" onClick={click} /></div>
+        <div className="col-4">{score}</div>
     </div>
   )
 }
@@ -112,7 +88,7 @@ function Result() {
   const [nilScore, setNilScore] = React.useState(0)
 
   return (
-    <div class="w-100 h-75 grid results row g-0">
+    <div className="w-100 h-75 grid results row g-0">
       <ResultBlock player='X' score={xScore} click={() => setXScore(xScore + 1)} />
       <ResultBlock player='=' score={nilScore} click={() => setNilScore(nilScore + 1)} />
       <ResultBlock player='O' score={oScore} click={() => setOScore(oScore + 1)} />
