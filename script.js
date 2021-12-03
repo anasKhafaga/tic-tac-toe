85e343ed8c5b2239fa52f72321388c6a5e9890ec
Anas Khafaga <anaskhafaga1810@gmail.com>
2021-12-03 21:44:25 +0200

    props

--- script.js
+++ script.js
@@ -3,54 +3,46 @@ const player1 = 'X'
 const player2 = 'O'
 let winner = 'X';
 
-function Block(){
+function Block({classProp}){
   return (
-    <div className="col-4 tl"></div>
+    <div className={classProp}></div>
   )
 }
 
 class Row1 extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
 
   render() {
+    const {classProp} = this.props
     return (
-      <div className="row g-0">
-          {/*<div className="col-4 tl"></div>
-          <div className="col-4 tm"></div>
-          <div className="col-4 tr"></div>*/}
-          <Block />
-          <Block />
-          <Block />
+      <div className={classProp}>
+          <Block classProp="col-4 tl" />
+          <Block classProp="col-4 tm" />
+          <Block classProp="col-4 tr" />
       </div>
     )
   }
 }
 
-function Row2() {
+function Row2({classProp}) {
 
   if(winner == 'X'){
     return (
-      <div className="row g-0">
-          {/*<div className="col-4 ml">{player1}</div>
-          <div className="col-4 mm">{player1}</div>
-          <div className="col-4 mr">{player1}</div>*/}
-          <Block />
-          <Block />
-          <Block />
+      <div className={classProp}>
+          <Block classProp="col-4 ml" />
+          <Block classProp="col-4 mm" />
+          <Block classProp="col-4 mr" />
       </div>
     )
 
   }else {
     return (
-      <div className="row g-0">
-          {/*<div className="col-4 ml">{player2}</div>
-          <div className="col-4 mm">{player2}</div>
-          <div className="col-4 mr">{player2}</div>*/}
-          <Block />
-          <Block />
-          <Block />
+      <div className={classProp}>
+          <Block classProp="col-4 ml" />
+          <Block classProp="col-4 mm" />
+          <Block classProp="col-4 mr" />
       </div>
     )
   }
@@ -58,19 +50,17 @@ function Row2() {
 };
 
 class Row3 extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
 
   render() {
+    const {classProp} = this.props
     return (
-      <div className="row g-0">
-          {/*<div className="col-4 bl"></div>
-          <div className="col-4 bm"></div>
-          <div className="col-4 br"></div>*/}
-          <Block />
-          <Block />
-          <Block />
+      <div className={classProp}>
+          <Block classProp="col-4 bl" />
+          <Block classProp="col-4 bm" />
+          <Block classProp="col-4 br" />
       </div>
     )
   }
@@ -84,9 +74,9 @@ class Board extends React.Component {
   render() {
     return(
       <div className="bk-color-2 w-100 h-75 grid board row g-0" id="board-root">
-        <Row1 />
-        <Row2 />
-        <Row3 />
+        <Row1 classProp="row g-0" />
+        <Row2 classProp="row g-0" />
+        <Row3 classProp="row g-0" />
       </div>
     )
   }
